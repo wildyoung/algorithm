5299b93ca318a72928444a38128ffae6fdd72b9a
송현민 <157588023+wildyoung@users.noreply.github.com>
2026-02-10 17:18:39 +0900

    Implement custom priority queue with absolute value comparison

---
+++ priority_queue/11286.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class cmp {
+public:
+	bool operator()(int a, int b)
+	{
+		if (abs(a) != abs(b)) return abs(a) > abs(b);
+		return a > 0 && b < 0;
+	}
+};
+int n;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	priority_queue<int, vector<int>, cmp> pq;
+
+	cin >> n; // 테스트케이스 입력
+
+	while (n--)
+	{
+		int x;
+		cin >> x;
+
+		if (x == 0)
+		{
+			if (pq.empty()) cout << "0\n";
+			else
+			{
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else pq.push(x);
+	}
+
+	return 0;
+}
